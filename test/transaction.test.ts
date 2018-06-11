d6f790325e5ef3eee5935053afb6b3c8753f7217
bin.qian <cola.tin.com@gmail.com>
2018-06-11 10:59:24 +0800

    test: more case for tx

--- test/transaction.test.ts
+++ test/transaction.test.ts
@@ -27,6 +27,15 @@ describe("transaction", () => {
         expect(unsigned.signingHash.toString()).equal('0x2a1c25ce0d66f45276a5f308b99bf410e2fc7d5b6ea37a49f2ab9f1da9446478')
         expect(unsigned.id).deep.equal(new Bytes32(Buffer.alloc(0)))
         expect(unsigned.intrinsicGas).equal(37432)
+        expect(new Transaction({ ...body, clauses: [] }).intrinsicGas).equal(21000)
+        expect(new Transaction({
+            ...body,
+            clauses: [{
+                to: null,
+                value: BigInt.from(0),
+                data: Buffer.alloc(0)
+            }]
+        }).intrinsicGas).equal(53000)
         expect(unsigned.gasPrice(BigInt.from(100))).deep.equal(BigInt.from(150))
         expect(unsigned.signature).to.be.undefined
         expect(() => { unsigned.signer }).to.throw()
@@ -63,6 +72,12 @@ describe("transaction", () => {
         expect(signed.signature).deep.equal(Buffer.from('f76f3c91a834165872aa9464fc55b03a13f46ea8d3b858e528fcceaf371ad6884193c3f313ff8effbb57fe4d1adc13dceb933bedbf9dbb528d2936203d5511df00', 'hex'))
         expect(signed.signer).deep.equal(signer)
         expect(signed.id).deep.equal(Bytes32.fromHex('0xda90eaea52980bc4bb8d40cb2ff84d78433b3b4a6e7d50b75736c5e3e77b71ec'))
+    })
+
+    let encoded = Buffer.from('f8970184aabbccdd20f840df947567d83b7b8d80addcb281a71d54fc7b3364ffed82271086000000606060df947567d83b7b8d80addcb281a71d54fc7b3364ffed824e208600000060606081808252088083bc614ec0b841f76f3c91a834165872aa9464fc55b03a13f46ea8d3b858e528fcceaf371ad6884193c3f313ff8effbb57fe4d1adc13dceb933bedbf9dbb528d2936203d5511df00', 'hex')
+    it("encode decode", () => {
+        expect(signed.encode()).deep.equal(encoded)
+        expect(Transaction.decode(encoded)).deep.equal(signed)
     })
 
     let incorrectlySigned = new Transaction(body)
